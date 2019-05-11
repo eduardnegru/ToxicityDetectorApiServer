d18564e01a50f5fbc9c78dadf63d6b01d3dd5827
adrian <adrian_eduard.negru>
2019-05-12 01:05:50 +0300

    fix

--- server.js
+++ server.js
@@ -3,26 +3,7 @@ const chalk = require("chalk");
 const app = express();
 const childProcess = require("child_process");
 
-function pull(res){
-	childProcess.exec('git pull', function(error, stdout, stderr){
-		console.log(stdout);
-		if (error)
-		{
-			console.error(error);
-			return res.send(500);
-		}
-		else
-		{
-			res.send(200);
-		}
-	});
-}
-
 app.get('/', (req, res) => res.send("It works!"));
-
-app.post('/webhooks/pull', (req, res) => {
-		pull(res);
-});
 
 app.listen(3000, () => {
 	console.log(chalk.yellow("server listening on 0.0.0.0:3000"));
