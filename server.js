a99f0ac46d6eed4e63ca0fe4d14a77aae8336df6
adrian <adrian_eduard.negru>
2019-05-12 00:37:53 +0300

    added webhook

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const chalk = require("chalk");
 const app = express();
 
 app.get('/', (req, res) => res.send("It works!"));
-
+app.get('/webhooks/pull', (req, res) => res.send("Nice!"));
 app.listen(3000, () => {
     console.log(chalk.yellow("server listening on 0.0.0.0:3000"));
 });
