f7aa046359851d68c069d11bf0b4b5f9f0102f16
adrian <adrian_eduard.negru>
2019-05-12 00:48:11 +0300

    fix

---
+++ push_all.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo git add .
+sudo git commit -m "fix"
+sudo git push origin master
--- server.js
+++ server.js
@@ -34,3 +34,6 @@ app.listen(3000, () => {
 	console.log(chalk.yellow("server listening on 0.0.0.0:3000"));
 });
 
+
+
+
