36e2cb8fd617f6baf0be4cddaf25489d53a7a1ec
adrian <adrian_eduard.negru>
2019-05-12 00:22:55 +0300

    fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const chalk = require("chalk");
 const app = express();
 
-app.get('/', (req, res) => res.send("I love you :* pwp"));
+app.get('/', (req, res) => res.send("It works!"));
 
 app.listen(3000, () => {
     console.log(chalk.yellow("server listening on 0.0.0.0:3000"));
