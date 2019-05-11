f9b73de16076f564c2bc961c32f7e0d5c0564f7d
adrian <adrian_eduard.negru>
2019-05-12 00:12:19 +0300

    text

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const chalk = require("chalk");
 const app = express();
 
-app.get('/', (req, res) => res.send("Hello World"));
+app.get('/', (req, res) => res.send("I love you :* pwp"));
 
 app.listen(3000, () => {
     console.log(chalk.yellow("server listening on 0.0.0.0:3000"));
