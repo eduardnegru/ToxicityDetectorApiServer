caae12268c7074002b1c80c1a41dd12daa2fec19
adrian <adrian_eduard.negru>
2019-05-12 22:40:38 +0300

    fix

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ async function run_prediction(req, res)
 	try
 	{
 		console.log(req.body);
+		console.log(req.body["text"]);
 		let strText = JSON.parse(Object.keys(req.body)[0])["text"];
 		let arrVectors = lstm_model_text_to_vector(strText);
 		let arrPrediction = await model.predict(tf.tensor([arrVectors])).array();
