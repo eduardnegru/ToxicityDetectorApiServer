ad0f5bf03b386d1fe23a16a28a20505717b831d2
adrian <adrian_eduard.negru>
2019-05-12 18:35:05 +0300

    fix

--- server.js
+++ server.js
@@ -75,6 +75,7 @@ async function run_prediction(req, res)
 	app.use(function(req, res, next) {
 		res.header("Access-Control-Allow-Origin", "*");
 		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		next();
 	});
 
 	app.get('/', (req, res) => res.send("It works!"));
