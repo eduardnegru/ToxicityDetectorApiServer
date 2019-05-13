f98ad81a2f3a76fc7cf006d8db06223103babc60
adrian <adrian_eduard.negru>
2019-05-13 21:49:14 +0300

    fix

--- server.py
+++ server.py
@@ -32,6 +32,9 @@ def read_embeddings():
 		coefs = np.asarray(values[1:-1])
 		# embeddings_index[word] = coefs
 		embeddings_index[word] = coefs.astype("float32")
+		if i < 1000:
+			break
+		i+=1
 	f.close()
 
 
